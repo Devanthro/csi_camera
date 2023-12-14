654b0ffc96066cfe3c278c202150cdbb1fe351c1
Justin Queiros de Oliveira <justin.oliveira2002@gmail.com>
2023-12-14 17:14:22 +0100

    somehow the folder was not pushed

---
+++ csi_camera/h265_package/camera_publisher.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import ByteMultiArray
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+import subprocess
+import threading
+import signal
+import sys
+
+def gstreamer_pipeline1(sensor_id=0, capture_width=1920, capture_height=1080, framerate=30, bitrate=8000000, flip_method=0):
+    return (
+        f"nvarguscamerasrc sensor-id={sensor_id} ! "
+        f"video/x-raw(memory:NVMM), width=(int){capture_width}, height=(int){capture_height}, format=(string)NV12, framerate=(fraction){framerate}/1 ! "
+        f"nvvidconv flip-method={flip_method} ! "
+        "video/x-raw(memory:NVMM), format=(string)NV12 ! "
+        f"nvv4l2h265enc bitrate={bitrate} ! h265parse ! "
+        "fdsink"
+    )
+
+class CameraNode(Node):
+    def __init__(self, **kwargs):
+        super().__init__("camera_node")
+        self.image_topic_ = self.declare_parameter("image_topic", "/image/encoded").value
+        self.image_publisher_ = self.create_publisher(ByteMultiArray, self.image_topic_, 1)
+        self.total_data_size = 0
+        self.message_count = 0
+
+        # Set up signal handler
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        # GStreamer setup for pipeline1
+        Gst.init(None)
+        self.pipeline1 = Gst.parse_launch(gstreamer_pipeline1())
+        self.fdsink1 = self.pipeline1.get_by_name('fdsink')
+        self.start_gstreamer_subprocess()
+
+    def start_gstreamer_subprocess(self):
+        cmd = ["gst-launch-1.0"] + gstreamer_pipeline1().split()
+        self.get_logger().info("Starting GStreamer subprocess with command: " + ' '.join(cmd))
+        self.gst_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
+        threading.Thread(target=self.read_gstreamer_output, daemon=True).start()
+
+    def read_gstreamer_output(self):
+        while self.gst_process.poll() is None:
+            data = self.gst_process.stdout.read(1000000)
+            if data:
+                msg = ByteMultiArray()
+                msg.data = [bytes([b]) for b in data]
+                self.image_publisher_.publish(msg)
+                data_size = len(msg.data)
+                self.total_data_size += data_size
+                self.message_count += 1
+                self.get_logger().info(f"Published data of size {data_size} bytes")
+
+    def signal_handler(self, signum, frame):
+        self.get_logger().info(f"Signal {signum} received, shutting down...")
+        self.close_videocapture()
+        self.destroy_node()
+        rclpy.shutdown()
+
+    def close_videocapture(self):
+        self.video_capture.release()
+        print(f"\nTotal data size transmitted: {self.total_data_size / (1024 * 1024)} MB")
+        print(f"Total number of messages transmitted: {self.message_count}")
+        self.pipeline1.set_state(Gst.State.NULL)
+
+def main(args=None):
+    rclpy.init(args=args)
+    camera_node = CameraNode()
+    signal.signal(signal.SIGINT, camera_node.signal_handler)
+    rclpy.spin(camera_node)
+    camera_node.close_gstreamer()
+    camera_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
---
+++ csi_camera/h265_package/camera_subscriber.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import ByteMultiArray
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+import numpy as np
+import cv2
+
+def gstreamer_pipeline2():
+    return (
+        "appsrc name=appsrc ! "
+        "h265parse ! "
+        "nvv4l2decoder ! "
+        "nvvidconv ! "
+        "video/x-raw, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! "
+        "appsink emit-signals=True name=appsink2"
+    )
+
+class DisplayNode(Node):
+    def __init__(self):
+        super().__init__("display_node")
+        self.image_topic_ = self.declare_parameter("image_topic", "/image/encoded").value
+        self.image_subscription_ = self.create_subscription(ByteMultiArray, self.image_topic_, self.image_callback, 1)
+
+        # GStreamer setup for pipeline2
+        Gst.init(None)
+        self.pipeline2 = Gst.parse_launch(gstreamer_pipeline2())
+        self.appsrc2 = self.pipeline2.get_by_name('appsrc')
+        self.appsink2 = self.pipeline2.get_by_name('appsink2')
+        self.appsink2.set_property('emit-signals', True)
+        self.appsink2.connect('new-sample', self.on_new_sample2)
+        self.pipeline2.set_state(Gst.State.PLAYING)
+
+    def image_callback(self, msg):
+        # Reconstruct the data from the list of bytes
+        reconstructed_data = b''.join(msg.data)
+
+        # Create a GStreamer buffer from the reconstructed data
+        buffer = Gst.Buffer.new_wrapped(reconstructed_data)
+        self.appsrc2.emit('push-buffer', buffer)
+
+    def on_new_sample2(self, appsink):
+        sample = appsink.emit('pull-sample')
+        if sample:
+            buffer = sample.get_buffer()
+            caps = sample.get_caps()
+            structure = caps.get_structure(0)
+            width = structure.get_value('width')
+            height = structure.get_value('height')
+
+            # Extract the frame
+            buffer = buffer.extract_dup(0, buffer.get_size())
+            frame = np.ndarray((height, width, 3), buffer=buffer, dtype=np.uint8)
+
+            # Display the frame
+            cv2.imshow("Captured Frame", frame)
+            cv2.waitKey(1)
+
+            return Gst.FlowReturn.OK
+
+def main(args=None):
+    rclpy.init(args=args)
+    display_node = DisplayNode()
+    rclpy.spin(display_node)
+
+    display_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
